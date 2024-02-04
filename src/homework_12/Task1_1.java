2ff49ba56cb9ff2fce8a5b15b2f3980d36c96184
Anzhelika <ztlav2023@gmail.com>
2024-02-04 15:27:16 +0100

    new

--- src/homework_12/Task1_1.java
+++ src/homework_12/Task1_1.java
@@ -1,64 +1,79 @@
 package homework_12;
-// Написать метод, принимающий на вход массив строк.
-// Метод должен вернуть массив, состоящий из самой короткой и самой длинной строки изначального массива.
+/*
+@date 01.02.2024
+@author Sergey Bugaienko
+*/
 
-public class Task1_1 {
+/*
+Написать метод, принимающий на вход массив строк.
+Метод должен вернуть массив, состоящий из самой короткой и самой длинной строки изначального массива.
+ */
+public class Task1_1{
     public static void main(String[] args) {
 
-        String[] strings = {"One", "Java", "Python", "butterfly", "js"};
-        System.out.println("============ Without null ==========");
 
+        String[] strings = {"One", "Java", "Python", "butterfly", "JS"};
+        System.out.println(" ============ Without null ==============");
         String[] res = arraySmallestAndLongestStrings(strings);
         printArray(res);
-        System.out.println("\n============ testArray = null ==========");
 
+
+        System.out.println(" \n============ testArray = null ==============");
 
         String[] testArray = null;
         res = arraySmallestAndLongestStrings(testArray);
         printArray(res);
 
-        System.out.println("\n============ null in array ==========");
+        System.out.println(" \n============ null in array ==============");
 
-        testArray = new String[]{"One", "Java", null, "Python", "butterfly", "js"};
+
+        testArray = new String[]{"One", "Java", "Python", null, "butterfly", "JS"};
         res = arraySmallestAndLongestStrings(testArray);
         printArray(res);
 
+        System.out.println(" \n============ first null in array ==============");
 
-        System.out.println("\n============ First null in array ==========");
 
-        testArray = new String[]{null, "One", "Java", "Python", "butterfly", "js"};
+        testArray = new String[]{null, null, null, null, null, null, null, "JS"};
+        testArray = new String[]{null, "One", "Java", "Python", null, "butterfly", "JS"};
         res = arraySmallestAndLongestStrings(testArray);
         printArray(res);
 
-        System.out.println("\n============ All null in array ==========");
-        testArray = new String[]{null, null, null, null, null, null};
+        System.out.println(" \n============ ALL null in array ==============");
+        testArray = new String[]{null, null, null, null, null, null, null};
         res = arraySmallestAndLongestStrings(testArray);
         printArray(res);
 
     }
 
-
     public static String[] arraySmallestAndLongestStrings(String[] strings) {
-        // Проверить, на null ссылку strings (а существует ли обьект массива)
-        // Надо найти строку наим и наибольшей длины
-        // Создать массив из двух строк. Записать туда найденные строки и вернуть
+
+        // 0. Проверить cсылку strings на null (а существует ли объект массива)
+        // 1. Надо найти строку наим и наибольшей длинный.
+        // 2. Создать массив из 2-х строк. Записать туда найденные строки и вернуть
 
         // Проблема null
-        /* 1. Если у нас нет null. Ссылка на массив - ок, все элементы в массиве не null
+        /*
+        1. Если у нас нет null. Ссылка на массив - ок, все элементы в массиве - не null
         Алгоритм будет корректно работать
 
-        2. Если ссылка на массив = null -> Мы возвращаем новый массив, заполненный значением по умолчанию (null)
-        {"", ""} null, null
+        2. Если ссылка на массив = null -> Мы возвращаем новый массив, заполненный значениями по умолчанию (null) {null, null}
 
-        3. Если в массиве присутствует элемент со значением null - нам надо обработать эту ситуацию. Иначе при попытке
-        получить длину такого элемента мы получим NPE (ошибка-null pointer exception)
+        3. Если в массиве присутствуют элементы со значением null - нам надо обработать эту ситуацию.
+        Иначе, при попытке получить длину такого элемента мы получим NPE
+        Решение - создаем отдельный метод. Перебираем наш массив - как только находим НЕ null значение - присваиваем его в мин и макс строку
+
+        4. Отдельный случай, требующий внимания - null в 0 элементе массива.
+
+
          */
         if (strings == null) {
             return new String[2]; // {null, null}
         }
 
+
         String minLen = findNotNullValue(strings);
-        String maxLen = strings[0];
+        String maxLen = minLen;
 
         for (int i = 0; i < strings.length; i++) {
             if (strings[i] != null && minLen != null && minLen.length() > strings[i].length()) {
@@ -73,7 +88,7 @@ public class Task1_1 {
         return new String[]{minLen, maxLen};
     }
 
-    public static String findNotNullValue(String[] strings) {
+    private static String findNotNullValue(String[] strings) {
 
         for (int i = 0; i < strings.length; i++) {
             if (strings[i] != null) {
@@ -82,15 +97,15 @@ public class Task1_1 {
         }
         return null;
     }
+
     public static void printArray(String[] strings) {
         System.out.print("[");
         for (int i = 0; i < strings.length; i++) {
-            System.out.print(strings[i] + ((i < strings.length - 1) ? ";" : "]\n"));
+            System.out.print(strings[i] + ((i < strings.length - 1) ? "; " : "]\n"));
+
         }
     }
 }
-
-
 
 
 
---
+++ src/homework_12/Task3_1.java
@@ -0,0 +1,46 @@
+package homework_12;
+
+public class Task3_1 {
+/*
+@date 01.02.2024
+@author Sergey Bugaienko
+*/
+
+    /*
+    на вход принимает массив целых чисел и число - длину нового массива.
+    Метод должен создать и распечатать массив заданной в параметрах длинны.
+    В начало массива должны быть скопированы элементы из входящего массива:
+
+    {0, 1, 2, 3, 4 , 5, 6} -> copyOfArray(array, 3) -> вывод на печать {0, 1, 2}
+     */
+
+        public static void main(String[] args) {
+            int[] testArr = {0, 1, 2, 3, 4, 5, 6};
+
+            copyOfArray(testArr, 3);
+
+
+        }
+
+        public static void copyOfArray(int[] ints, int newLength) {
+            int[] result = new int[newLength];
+
+
+            for (int i = 0; i < result.length && i < ints.length; i++) {
+                result[i] = ints[i];
+            }
+
+            printArray(result);
+
+
+        }
+
+        public static void printArray(int[] ints) {
+            System.out.print("[");
+            for (int i = 0; i < ints.length; i++) {
+                System.out.print(ints[i] + ((i < ints.length - 1) ? "; " : "]\n"));
+
+            }
+        }
+    }
+
